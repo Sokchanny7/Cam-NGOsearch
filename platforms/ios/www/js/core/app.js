7472f18ab66062b3fe286d97fa9cd5a788847bcc
khmerupdateku <khmerupdateku@gmail.com>
2015-11-09 14:40:41 +0700

    Add Platform IOS

---
+++ platforms/ios/www/js/core/app.js
@@ -0,0 +1,12 @@
+var App = {
+  initialize: function () {
+    this.bindEvents();
+  },
+  bindEvents: function () {
+    document.addEventListener('deviceready', this.onDeviceReady, false);
+  },
+  onDeviceReady: function () {
+   AppCache.clearAll();
+  }
+};
+App.initialize();
